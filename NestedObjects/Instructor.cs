a89e839fed756973c8c5f400cf13184f51ce5e9c
Obe <o.bdondo@gmail.com>
2020-07-14 17:46:02 -0700

    Added Instructor class

---
+++ NestedObjects/Instructor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    class Instructor
+    {
+        /// <summary>
+        /// Legal Full name. First and last.
+        /// Example: Jane Doe
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Work email
+        /// Example: janedoe@cptc.edu
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
